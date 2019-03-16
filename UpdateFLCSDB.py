7b40592f06d18c1b061d6f731625aa419f5f69e7
alexikerd <41974772+alexikerd@users.noreply.github.com>
2019-03-16 17:09:50 -0500

    Add files via upload

--- UpdateFLCSDB.py
+++ UpdateFLCSDB.py
@@ -3,32 +3,37 @@ import pandas as pd
 from os import path
 from sqlalchemy import *
 
-FLCS_Path = path.abspath(path.curdir)
-
 e = create_engine('mysql://root:password@localhost:3306/flcs')
 conn = e.connect()
 cur = conn.connection.cursor()
 
-rawdata = pd.read_csv(FLCS_Path + r"\FLCS.csv", delimiter=',',names = ("Split","Player","Position","Team","Win/Loss","Points"),usecols=(0,1,3,4,5,6))
-rawdata['Week'] = ''
-rawdata['Opponent'] = rawdata['Team'].astype(str).str[-3:]
-rawdata['Excess'] = rawdata['Position']
-rawdata['Win/Loss'] = rawdata['Win/Loss'].astype(str).str[0]
-rawdata['Team'] = rawdata['Team'].astype(str).str[:3]
-rawdata = rawdata[["Split","Week","Player","Position","Team","Opponent","Win/Loss","Points","Excess"]]
-rawdata['Week'] = rawdata['Player'].astype(str).str[-1:]
-rawdata['Position'] = rawdata['Position'].fillna(method='ffill')
-a = 1
-week = []
-replace = []
-while (a < 10):
-    week.append("Week %d" % a)
-    replace.append(np.nan)
-    a = a + 1
-rawdata['Player'] = rawdata['Player'].replace(week,replace)
-rawdata['Player'] = rawdata['Player'].fillna(method='ffill')
-rawdata = rawdata[rawdata['Excess'].isna()]
-rawdata = rawdata.drop(columns = ['Excess'])
+
+FLCS_Path = path.abspath(path.curdir)
+scrapedata = pd.read_csv(FLCS_Path + r"\Split1.csv", delimiter=',',names = ("Player","Week","Position","Team","Points","Excess"),usecols=(0,1,2,3,4,12))
+scrapedata['Player1'] = ''
+scrapedata['Week1'] = ''
+scrapedata['Position1'] = ''
+scrapedata['Team1'] = ''
+scrapedata['Points1'] = ''
+small = scrapedata[scrapedata['Excess'].isna()].shift(periods=6,axis='columns')
+big = scrapedata[scrapedata['Excess'].notnull()]
+complete = pd.concat([small,big]).reindex(scrapedata.index)
+complete['Position'] = complete['Position'].fillna(method='ffill')
+complete['Player'] = complete['Player'].fillna(method='ffill')
+complete = complete[complete['Points'].isna()]
+complete = complete.drop(['Week','Team','Points','Excess','Player1'],axis=1)
+complete['Split'] = 1
+complete['Week2'] = complete['Week1'].astype(str).str[-1:]
+complete['Week'] = complete['Week2'].astype(int) + ((complete['Split'] - 1) * 9)
+complete['Team'] = complete['Position1'].astype(str).str[:3]
+complete['Opponent'] = complete['Position1'].astype(str).str[-3:]
+complete['Points'] = complete['Points1']
+complete['Result'] = complete['Team1'].astype(str).str[0]
+complete = complete.drop(['Week1','Team1','Points1','Position1','Week2'],axis=1)
+complete = complete[["Split","Week","Player","Position","Team","Opponent","Result","Points"]]
+complete = complete[complete['Points'] != "0.00"]
+complete
 
 cur.execute("DROP TABLE IF EXISTS player")
-rawdata.to_sql(name='player',con=e,dtype={"Split": Float, "Week": Float, "Player": String(32), "Position": String(32), "Team": String(32), "Opponent": String(32), "Win/Loss": String(32), "Points": Float})
+complete.to_sql(name='player',con=e,dtype={"Split": Float, "Week": Float, "Player": String(32), "Position": String(32), "Team": String(32), "Opponent": String(32), "Result": String(32), "Points": Float})
+
