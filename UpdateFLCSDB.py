d72c6ab8c42bdf7f958466513164d8a7bf3897ed
alexikerd <41974772+alexikerd@users.noreply.github.com>
2019-04-29 20:06:42 -0500

    Add files via upload

--- UpdateFLCSDB.py
+++ UpdateFLCSDB.py
@@ -58,6 +58,310 @@ total['Team'] = total['Team'].str.strip()
 total['Opponent'] = total['Opponent'].str.strip()
 
 
+playerroles = total[['Player','Position']].drop_duplicates()
+total4 = pd.DataFrame(columns=["Week","Player","Team","Opponent","Region","Points"])
+Week = pd.read_excel("Split8.xlsx",sheet_name="Week1")
+WeekTeams = Week[Week["Game 1"].isna()]
+WeekPlayers = Week[(Week['Players'].str.isupper()==False)&(Week['Players'] != 100)]
+WeekPlayers[['Points 1','Game 1']] = WeekPlayers['Game 1'].str.split('(',expand=True)
+WeekPlayers[['Points 2','Game 2']] = WeekPlayers['Game 2'].str.split('(',expand=True)
+WeekPlayers['Team 1.1'] = WeekPlayers['Game 1'].astype(str).str[:3]
+WeekPlayers['Team 2.1'] = WeekPlayers['Game 2'].astype(str).str[:3]
+WeekPlayers['Team 1.1'] = WeekPlayers['Team 1.1'].str.rstrip('-')
+WeekPlayers['Team 2.1'] = WeekPlayers['Team 2.1'].str.rstrip('-')
+WeekPlayers['Team 1.2'] = WeekPlayers['Game 1'].astype(str).str[-4:]
+WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.lstrip('-')
+WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.rstrip(')')
+WeekPlayers['Team 2.2'] = WeekPlayers['Game 2'].astype(str).str[-4:]
+WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.lstrip('-')
+WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.rstrip(')')
+WeekPlayers = WeekPlayers.reset_index(drop=True)
+i=0
+while (i<WeekPlayers.shape[0]):
+    if (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+    elif (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+    elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+    elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+    i+=1
+        
+WeekPlayers1 = WeekPlayers[['Players','Team 1','Opponent 1','Points 1']].rename(columns={'Players':'Player','Team 1': 'Team','Opponent 1':'Opponent','Points 1':'Points'})
+WeekPlayers2 = WeekPlayers[['Players','Team 2','Opponent 2','Points 2']].rename(columns={'Players':'Player','Team 2': 'Team','Opponent 2':'Opponent','Points 2':'Points'})       
+WeekPlayers = WeekPlayers1.append(WeekPlayers2,sort='True')        
+WeekPlayers['Week'] = 64
+WeekPlayers['Region'] = 'EU'
+WeekPlayers = WeekPlayers.reset_index(drop=True)
+WeekPlayers = WeekPlayers[['Week','Player','Team','Opponent','Region','Points']]
+total4 = total4.append(WeekPlayers,sort=False)
+
+
+Week = pd.read_excel("Split8.xlsx",sheet_name="Week2")
+Week = Week.iloc[(Week[Week['Players'].isna()].index[0]+ 1):]
+Week = Week[Week['Players'].notna()]
+WeekPlayers = Week[(Week['Players'].str.isupper()==False)&(Week['Players'] != 100)]
+WeekPlayers[['Points 1','Game 1']] = WeekPlayers['Game 1'].str.split('(',expand=True)
+WeekPlayers[['Points 2','Game 2']] = WeekPlayers['Game 2'].str.split('(',expand=True)
+WeekPlayers['Team 1.1'] = WeekPlayers['Game 1'].astype(str).str[:3]
+WeekPlayers['Team 2.1'] = WeekPlayers['Game 2'].astype(str).str[:3]
+WeekPlayers['Team 1.1'] = WeekPlayers['Team 1.1'].str.rstrip('-')
+WeekPlayers['Team 2.1'] = WeekPlayers['Team 2.1'].str.rstrip('-')
+WeekPlayers['Team 1.2'] = WeekPlayers['Game 1'].astype(str).str[-4:]
+WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.lstrip('-')
+WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.rstrip(')')
+WeekPlayers['Team 2.2'] = WeekPlayers['Game 2'].astype(str).str[-4:]
+WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.lstrip('-')
+WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.rstrip(')')
+WeekPlayers = WeekPlayers.reset_index(drop=True)
+i=0
+while (i<WeekPlayers.shape[0]):
+    if (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+    elif (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+    elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+    elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+        WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+        WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+        WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+    
+    i+=1
+        
+WeekPlayers1 = WeekPlayers[['Players','Team 1','Opponent 1','Points 1']].rename(columns={'Players':'Player','Team 1': 'Team','Opponent 1':'Opponent','Points 1':'Points'})
+WeekPlayers2 = WeekPlayers[['Players','Team 2','Opponent 2','Points 2']].rename(columns={'Players':'Player','Team 2': 'Team','Opponent 2':'Opponent','Points 2':'Points'})       
+WeekPlayers = WeekPlayers1.append(WeekPlayers2,sort='True')        
+WeekPlayers['Week'] = 64
+WeekPlayers['Region'] = 'NA'
+WeekPlayers = WeekPlayers[['Week','Player','Team','Opponent','Region','Points']]
+WeekPlayers = WeekPlayers[WeekPlayers['Points'] != '']
+WeekPlayers = WeekPlayers.reset_index(drop=True)
+total4 = total4.append(WeekPlayers,sort=False)
+total4 = total4.reset_index(drop=True)
+
+j = 2
+while(j<10):
+    Week = pd.read_excel("Split8.xlsx",sheet_name="Week" + str(j))
+    Week = Week.iloc[:(Week[Week['Players'].isna()].index[0])]
+    Week = Week[Week['Players'].notna()]
+    WeekPlayers = Week[(Week['Players'].str.isupper()==False)&(Week['Players'] != 100)]
+    WeekPlayers[['Points 1','Game 1']] = WeekPlayers['Game 1'].str.split('(',expand=True)
+    WeekPlayers[['Points 2','Game 2']] = WeekPlayers['Game 2'].str.split('(',expand=True)
+    WeekPlayers['Team 1.1'] = WeekPlayers['Game 1'].astype(str).str[:3]
+    WeekPlayers['Team 2.1'] = WeekPlayers['Game 2'].astype(str).str[:3]
+    WeekPlayers['Team 1.1'] = WeekPlayers['Team 1.1'].str.rstrip('-')
+    WeekPlayers['Team 2.1'] = WeekPlayers['Team 2.1'].str.rstrip('-')
+    WeekPlayers['Team 1.2'] = WeekPlayers['Game 1'].astype(str).str[-4:]
+    WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.lstrip('-')
+    WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.rstrip(')')
+    WeekPlayers['Team 2.2'] = WeekPlayers['Game 2'].astype(str).str[-4:]
+    WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.lstrip('-')
+    WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.rstrip(')')
+    WeekPlayers = WeekPlayers.reset_index(drop=True)
+    i=0
+    while (i<WeekPlayers.shape[0]):
+        if (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+        elif (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+        elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+        elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+
+        i+=1
+
+    WeekPlayers1 = WeekPlayers[['Players','Team 1','Opponent 1','Points 1']].rename(columns={'Players':'Player','Team 1': 'Team','Opponent 1':'Opponent','Points 1':'Points'})
+    WeekPlayers2 = WeekPlayers[['Players','Team 2','Opponent 2','Points 2']].rename(columns={'Players':'Player','Team 2': 'Team','Opponent 2':'Opponent','Points 2':'Points'})       
+    WeekPlayers = WeekPlayers1.append(WeekPlayers2,sort='True')        
+    WeekPlayers['Week'] = 63 + j
+    WeekPlayers['Region'] = 'EU'
+    WeekPlayers = WeekPlayers[['Week','Player','Team','Opponent','Region','Points']]
+    WeekPlayers = WeekPlayers[WeekPlayers['Points'] != '']
+    WeekPlayers = WeekPlayers.reset_index(drop=True)
+    total4 = total4.append(WeekPlayers,sort=False)
+    total4 = total4.reset_index(drop=True)
+    
+    Week = pd.read_excel("Split8.xlsx",sheet_name="Week" + str(j+1))
+    Week = Week.iloc[(Week[Week['Players'].isna()].index[0]+ 1):]
+    Week = Week[Week['Players'].notna()]
+    WeekPlayers = Week[(Week['Players'].str.isupper()==False)&(Week['Players'] != 100)]
+    WeekPlayers[['Points 1','Game 1']] = WeekPlayers['Game 1'].str.split('(',expand=True)
+    WeekPlayers[['Points 2','Game 2']] = WeekPlayers['Game 2'].str.split('(',expand=True)
+    WeekPlayers['Team 1.1'] = WeekPlayers['Game 1'].astype(str).str[:3]
+    WeekPlayers['Team 2.1'] = WeekPlayers['Game 2'].astype(str).str[:3]
+    WeekPlayers['Team 1.1'] = WeekPlayers['Team 1.1'].str.rstrip('-')
+    WeekPlayers['Team 2.1'] = WeekPlayers['Team 2.1'].str.rstrip('-')
+    WeekPlayers['Team 1.2'] = WeekPlayers['Game 1'].astype(str).str[-4:]
+    WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.lstrip('-')
+    WeekPlayers['Team 1.2'] = WeekPlayers['Team 1.2'].str.rstrip(')')
+    WeekPlayers['Team 2.2'] = WeekPlayers['Game 2'].astype(str).str[-4:]
+    WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.lstrip('-')
+    WeekPlayers['Team 2.2'] = WeekPlayers['Team 2.2'].str.rstrip(')')
+    WeekPlayers = WeekPlayers.reset_index(drop=True)
+    i=0
+    while (i<WeekPlayers.shape[0]):
+        if (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+        elif (WeekPlayers['Team 1.1'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+        elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.1'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.2'].iloc[i])
+        elif (WeekPlayers['Team 1.2'].iloc[i] == WeekPlayers['Team 2.2'].iloc[i]):
+            WeekPlayers.set_value(i,'Team 1',WeekPlayers['Team 1.2'].iloc[i])
+            WeekPlayers.set_value(i,'Team 2',WeekPlayers['Team 2.2'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 1',WeekPlayers['Team 1.1'].iloc[i])
+            WeekPlayers.set_value(i,'Opponent 2',WeekPlayers['Team 2.1'].iloc[i])
+
+        i+=1
+
+    WeekPlayers1 = WeekPlayers[['Players','Team 1','Opponent 1','Points 1']].rename(columns={'Players':'Player','Team 1': 'Team','Opponent 1':'Opponent','Points 1':'Points'})
+    WeekPlayers2 = WeekPlayers[['Players','Team 2','Opponent 2','Points 2']].rename(columns={'Players':'Player','Team 2': 'Team','Opponent 2':'Opponent','Points 2':'Points'})       
+    WeekPlayers = WeekPlayers1.append(WeekPlayers2,sort='True')        
+    WeekPlayers['Week'] = 63 + j
+    WeekPlayers['Region'] = 'NA'
+    WeekPlayers = WeekPlayers[['Week','Player','Team','Opponent','Region','Points']]
+    WeekPlayers = WeekPlayers[WeekPlayers['Points'] != '']
+    WeekPlayers = WeekPlayers.reset_index(drop=True)
+    total4 = total4.append(WeekPlayers,sort=False)
+    total4 = total4.reset_index(drop=True)
+    j += 1
+
+total4 = total4.merge(playerroles, on='Player',how='left')
+total4['Result'] = ''
+total4['Split'] = 8
+total4 = total4[["Split","Week","Player","Position","Team","Opponent","Result","Points"]]
+
+
+
+total = total.append(total4,sort=false)
+
+total = total[["Split","Week","Player","Position","Team","Opponent","Result","Points"]]
+
+total[total['Player']== 'Soligo'] = total[total['Player']== 'Soligo'].assign(Position = 'Mid')
+total[total['Player']== 'Bang'] = total[total['Player']== 'Bang'].assign(Position = 'AD Carry')
+total[total['Player']== 'Fragas'] = total[total['Player']== 'Fragas'].assign(Position = 'Jungler')
+total[total['Player']== 'Soligo'] = total[total['Player']== 'Soligo'].assign(Position = 'Mid')
+total[total['Player']== 'FakeGod'] = total[total['Player']== 'FakeGod'].assign(Position = 'Top')
+total[total['Player']== 'Crown'] = total[total['Player']== 'Crown'].assign(Position = 'Mid')
+total[total['Player']== 'Broken Blade'] = total[total['Player']== 'Broken Blade'].assign(Position = 'Top')
+total[(total['Player']== 'Piglet') & (total['Week']== 72)] = total[(total['Player']== 'Piglet') & (total['Week']== 72)].assign(Position = 'Mid')
+total[total['Player']== 'Diamond'] = total[total['Player']== 'Diamond'].assign(Position = 'Support')
+total[total['Player']== 'V1per'] = total[total['Player']== 'V1per'].assign(Position = 'Top')
+total[total['Player']== 'GorillA'] = total[total['Player']== 'GorillA'].assign(Position = 'Support')
+total[total['Player']== 'Humanoid'] = total[total['Player']== 'Humanoid'].assign(Position = 'Mid')
+total[total['Player']== 'Exile'] = total[total['Player']== 'Exile'].assign(Position = 'Mid')
+total[total['Player']== 'Mowgli'] = total[total['Player']== 'Mowgli'].assign(Position = 'Jungler')
+total[total['Player']== 'Jeskla'] = total[total['Player']== 'Jeskla'].assign(Position = 'AD Carry')
+total[total['Player']== 'Nemesis'] = total[total['Player']== 'Nemesis'].assign(Position = 'Mid')
+total[total['Player']== 'Patrik'] = total[total['Player']== 'Patrik'].assign(Position = 'AD Carry')
+total[total['Player']== 'Crownshot'] = total[total['Player']== 'Crownshot'].assign(Position = 'AD Carry')
+total[total['Player']== 'Abbedagge'] = total[total['Player']== 'Abbedagge'].assign(Position = 'Mid')
+total[total['Player']== 'Selfmade'] = total[total['Player']== 'Selfmade'].assign(Position = 'Jungler')
+total[total['Player']== 'Asta'] = total[total['Player']== 'Asta'].assign(Position = 'AD Carry')
+total[total['Player']== 'FallenBandit'] = total[total['Player']== 'FallenBandit'].assign(Position = 'Top')
+total[total['Player']== 'Special'] = total[total['Player']== 'Special'].assign(Position = 'Mid')
+total[total['Player']== 'Finn'] = total[total['Player']== 'Finn'].assign(Position = 'Top')
+total[total['Player']== 'Kasing'] = total[total['Player']== 'FallenBandit'].assign(Position = 'Support')
+total[total['Player']== 'Orome'] = total[total['Player']== 'Orome'].assign(Position = 'Top')
+total[total['Player']== 'Panda'] = total[total['Player']== 'Panda'].assign(Position = 'Jungler')
+total[total['Player']== 'Saken'] = total[total['Player']== 'Saken'].assign(Position = 'Mid')
+total[total['Player']== 'Auto'] = total[total['Player']== 'Auto'].assign(Position = 'AD Carry')
+total[total['Player']== 'Mystiques'] = total[total['Player']== 'Mystiques'].assign(Position = 'Support')
+total[total['Player']== 'Promisq'] = total[total['Player']== 'Promisq'].assign(Position = 'Support')
+total[total['Player']== 'Kumo'] = total[total['Player']== 'Kumo'].assign(Position = 'Top')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 total2 = total2.drop(['Excess'],axis=1)
 total2['Opponent'] = complete2['Team'].astype(str).str[-3:]
 total2['Team'] = total2['Team'].astype(str).str[:3]
@@ -72,14 +376,15 @@ b = a['Week'].astype(str) + a['Team'] + a['Opponent']
 c = a['Week'].astype(str) + a['Opponent'] + a['Team']
 d = 0
 i = 0
-while (i < a.shape[0]): 
+while (i < len(c)): 
     if (b[b==c.iloc[i]].shape[0] > 0):
-        b = b[b!=b.iloc[d]]
         a = a[a['Week'].astype(str) + a['Team'] + a['Opponent'] != b.iloc[d]]
+        b = b[b!=b.iloc[d]]
         i += 1
     else:
         d += 1
         i += 1
+
 
 
 total3 = a
@@ -88,6 +393,6 @@ cur.execute("DROP TABLE IF EXISTS player")
 total.to_sql(name='player',con=e,dtype={"Split": Float, "Week": Float, "Player": String(32), "Position": String(32), "Team": String(32), "Opponent": String(32), "Result": String(32), "Points": Float})
 cur.execute("DROP TABLE IF EXISTS team")
 total2.to_sql(name='team',con=e,dtype={"Split": Float, "Team": String(32), "Opponent": String(32), "Result": String(32), "Points": Float})
-#cur.execute("DROP TABLE IF EXISTS results")
-#total2.to_sql(name='results',con=e,dtype={"Split": Float, "Week": String(32), "Team": String(32), "Opponent": String(32), "Result": String(32)})
+cur.execute("DROP TABLE IF EXISTS results")
+total3.to_sql(name='results',con=e,dtype={"Split": Float, "Week": String(32), "Team": String(32), "Opponent": String(32), "Result": String(32)},index=False)
 
